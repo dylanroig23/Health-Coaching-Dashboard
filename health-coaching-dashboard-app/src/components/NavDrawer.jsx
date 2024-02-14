fa9f064a065f1af92adf51e2bde5e8c0ccf85ca5
Dylan Roig <roig.10@buckeyemail.osu.edu>
2024-02-14 15:05:17 -0500

    Finalize NavDrawer Styling
    NavDrawer styling updated to mimic mockup design

--- health-coaching-dashboard-app/src/components/NavDrawer.jsx
+++ health-coaching-dashboard-app/src/components/NavDrawer.jsx
@@ -1,10 +1,75 @@
 import React from "react";
-import { Drawer, Box, Typography, IconButton } from "@mui/material";
+import { Drawer, Box, IconButton, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import EmojiPeopleIcon from "@mui/icons-material/EmojiPeople";
+import ContactsIcon from "@mui/icons-material/Contacts";
+import EditNoteIcon from "@mui/icons-material/EditNote";
 
 const NavDrawer = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const sidePanelItems = () => (
+    <Box
+      p={2}
+      width="350px"
+      textAlign="center"
+      role="presentation"
+      sx={{
+        borderLeft: `8px solid ${"#548235"}`,
+        height: "100%",
+        bgcolor: "#CAF2C9",
+      }}
+    >
+      <nav aria-label="additional nav links">
+        <List sx={{ color: "#548235" }}>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <ContactsIcon style={{ fontSize: 32, color: "#548235" }} />
+              </ListItemIcon>
+              <ListItemText
+                primary={
+                  <Typography variant="button">Contact Information</Typography>
+                }
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <EditNoteIcon style={{ fontSize: 42, color: "#548235" }} />
+              </ListItemIcon>
+              <ListItemText
+                primary={
+                  <Typography variant="button">REDCap Survey</Typography>
+                }
+              />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <EmojiPeopleIcon style={{ fontSize: 42, color: "#548235" }} />
+              </ListItemIcon>
+              <ListItemText
+                primary={
+                  <Typography variant="button">
+                    Long-Term SMART Goals
+                  </Typography>
+                }
+              />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Box>
+  );
 
   return (
     <>
@@ -14,6 +79,7 @@ const NavDrawer = () => {
         color="inherit"
         aria-label="logo"
         onClick={() => setIsDrawerOpen(true)}
+        sx={{ border: `2px solid ${"#548235"}`, bgcolor: "white" }}
       >
         <MenuIcon />
       </IconButton>
@@ -22,11 +88,7 @@ const NavDrawer = () => {
         open={isDrawerOpen}
         onClose={() => setIsDrawerOpen(false)}
       >
-        <Box p={2} width="250px" textAlign="center" role="presentation">
-          <Typography variant="h6" component="div">
-            Side Panel
-          </Typography>
-        </Box>
+        {sidePanelItems()}
       </Drawer>
     </>
   );
