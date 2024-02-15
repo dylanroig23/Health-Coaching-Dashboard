8d37d4cc3e57ebf430d8bf10cd51bdbe3016a162
Dylan Roig <roig.10@buckeyemail.osu.edu>
2024-02-15 12:36:15 -0500

    added scripts and add user button
    add user button not functional but displays when the database is empty

--- health-coaching-dashboard-app/src/pages/WeeklyDashboard.jsx
+++ health-coaching-dashboard-app/src/pages/WeeklyDashboard.jsx
@@ -1,58 +1,114 @@
-import React from 'react'
-import PageHeading from '../components/PageHeading'
-import NavBar from '../components/NavBar'
-import Container from '@mui/material/Container';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Unstable_Grid2';
+import React from "react";
+import PageHeading from "../components/PageHeading";
+import NavBar from "../components/NavBar";
+import Container from "@mui/material/Container";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
-  border: `1px solid ${'#548235'}`,
+  border: `1px solid ${"#548235"}`,
 }));
 
 const WeeklyDashboard = () => {
-  return (
-    <>
-      <PageHeading headingText="Welcome to User's Weekly Overview"/>
-      <NavBar />
-      <Container maxWidth="xl">
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={2}>
-            <Grid xs={4}>
-              <Item elevation={4}>Hours of Sleep</Item>
-            </Grid>
-            <Grid xs={4}>
-              <Item elevation={4}>Zone Minutes</Item>
-            </Grid>
-            <Grid xs={4}>
-              <Item elevation={4}>Sleep vs Zone</Item>
-            </Grid>
-            <Grid xs={4}>
-              <Item elevation={4}>Steps</Item>
-            </Grid>
-            <Grid xs={4}>
-              <Item elevation={4}>Servings of Fruits and Vegetables</Item>
-            </Grid>
-            <Grid xs={4}>
-              <Item elevation={4}>Healthy Meals</Item>
-            </Grid>
-            <Grid xs={4}>
-              <Item elevation={4}>Activity Completion</Item>
-            </Grid>
-            <Grid xs={4}>
-              <Item elevation={4}>User's Weekly SMART Goals</Item>
-            </Grid>
-          </Grid>
-        </Box>
-      </Container>
-    </>
-  )
-}
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState(null);
 
-export default WeeklyDashboard
+  const fetchData = async () => {
+    try {
+      const userResponse = await fetch("http://localhost:5000/users");
+      const userData = await userResponse.json();
+
+      if (userData && userData.length > 0) {
+        setUserData(userData);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); // Empty dependency array ensures this effect runs once on mount
+
+  if (userData) {
+    return (
+      <>
+        <PageHeading headingText="Welcome to User's Weekly Overview" />
+        <NavBar />
+        <Container maxWidth="xl">
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+              <Grid xs={4}>
+                <Item elevation={4}>Hours of Sleep</Item>
+              </Grid>
+              <Grid xs={4}>
+                <Item elevation={4}>Zone Minutes</Item>
+              </Grid>
+              <Grid xs={4}>
+                <Item elevation={4}>Sleep vs Zone</Item>
+              </Grid>
+              <Grid xs={4}>
+                <Item elevation={4}>Steps</Item>
+              </Grid>
+              <Grid xs={4}>
+                <Item elevation={4}>Servings of Fruits and Vegetables</Item>
+              </Grid>
+              <Grid xs={4}>
+                <Item elevation={4}>Healthy Meals</Item>
+              </Grid>
+              <Grid xs={4}>
+                <Item elevation={4}>Activity Completion</Item>
+              </Grid>
+              <Grid xs={4}>
+                <Item elevation={4}>User's Weekly SMART Goals</Item>
+              </Grid>
+            </Grid>
+          </Box>
+        </Container>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <PageHeading headingText="Please Add a User" />;
+        <Container
+          maxWidth="sm" // Limiting container width for centering purposes
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Button
+            variant="contained"
+            size="large" // Making the button larger
+            sx={{
+              marginTop: 2,
+              bgcolor: "#a9d18e",
+              border: `2px solid ${"#548235"}`,
+              "&:hover": {
+                bgcolor: "#548235",
+              },
+            }} // Adding some top margin for spacing
+            onClick={() => navigate("/authorize")}
+          >
+            Add User
+          </Button>
+        </Container>
+      </>
+    );
+  }
+};
+
+export default WeeklyDashboard;
---
+++ health-coaching-dashboard-app/src/scripts/fitbitAuth.js
@@ -0,0 +1,80 @@
+async function generateRandomCodeVerifier() {
+  const codeVerifierLength = 128;
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
+  let codeVerifier = "";
+
+  for (let i = 0; i < codeVerifierLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    codeVerifier += characters.charAt(randomIndex);
+  }
+
+  return codeVerifier;
+}
+
+async function generateCodeChallenge(codeVerifier) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(codeVerifier);
+  const hashedBuffer = await crypto.subtle.digest("SHA-256", data);
+  const hashedCodeVerifier = arrayBufferToBase64Url(hashedBuffer);
+  return hashedCodeVerifier;
+}
+
+function arrayBufferToBase64Url(buffer) {
+  const bytes = new Uint8Array(buffer);
+  const binary = String.fromCharCode(...bytes);
+  return btoa(binary)
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+}
+
+export async function generateCodeVerifierAndChallenge() {
+  const codeVerifier = await generateRandomCodeVerifier();
+  const codeChallenge = await generateCodeChallenge(codeVerifier);
+
+  return { codeVerifier, codeChallenge };
+}
+
+export function buildFitbitAuthorizationUrl(clientId, codeChallenge, scopes) {
+  const authorizationUrl = "https://www.fitbit.com/oauth2/authorize";
+  const responseType = "code";
+  const codeChallengeMethod = "S256";
+
+  const url = new URL(authorizationUrl);
+  url.searchParams.append("client_id", clientId);
+  url.searchParams.append("response_type", responseType);
+  url.searchParams.append("code_challenge", codeChallenge);
+  url.searchParams.append("code_challenge_method", codeChallengeMethod);
+  url.searchParams.append("scope", scopes.join(" "));
+
+  return url.toString();
+}
+
+export async function exchangeAuthorizationCodeForTokens(
+  clientId,
+  authorizationCode,
+  codeVerifier
+) {
+  const getTokensUrl = "https://api.fitbit.com/oauth2/token";
+  const grantType = "authorization_code";
+
+  const url = new URL(getTokensUrl);
+  url.searchParams.append("client_id", clientId);
+  url.searchParams.append("code", authorizationCode);
+  url.searchParams.append("code_verifier", codeVerifier);
+  url.searchParams.append("grant_type", grantType);
+
+  //API Post
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  const data = await res.json();
+
+  // will need to change this to get the refresh token as well
+  return data.access_token;
+}
