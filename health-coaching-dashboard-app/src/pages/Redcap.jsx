7754834d981b5a62a25151d874e31c676c4fa76a
Noverbey <112339061+Noverbey@users.noreply.github.com>
2024-02-19 21:50:00 -0500

    Added initial REDcap survey framework
    Added initial REDcap survey framework

---
+++ health-coaching-dashboard-app/src/pages/Redcap.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import SurveyComponent from "../components/SurveyComponent";
+
+const Redcap = ({ CLIENT_ID }) => {
+    return (
+      <>
+        <div>Redcap Page</div>
+        <h1>HELLO</h1>
+        <SurveyComponent />
+      </>
+    );
+}
+  
+  export default Redcap;
