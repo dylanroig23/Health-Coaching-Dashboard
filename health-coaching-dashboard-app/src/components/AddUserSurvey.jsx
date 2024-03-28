0fd0e1400b3a428d3b492edee2bac797db9a9136
Dylan Roig <roig.10@buckeyemail.osu.edu>
2024-03-28 13:30:25 -0400

    Add User in Survey Component
    add user posts user to the database

--- health-coaching-dashboard-app/src/components/AddUserSurvey.jsx
+++ health-coaching-dashboard-app/src/components/AddUserSurvey.jsx
@@ -4,6 +4,7 @@ import { Survey } from "survey-react-ui";
 import "survey-core/defaultV2.min.css";
 import { AddUserJson } from "../survey json/AddUserJson";
 //import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const AddUserSurvey = () => {
   const survey = new Model(AddUserJson);
@@ -11,6 +12,38 @@ const AddUserSurvey = () => {
 
   survey.onComplete.add((sender, options) => {
     console.log(JSON.stringify(sender.data, null, 3));
+    const dataJSON = sender.data;
+
+    const axiosPostData = async () => {
+      const postData = {
+        name: dataJSON.firstName,
+        startDate: "15-02-2024", //this should be today's date
+        fitbitUsername: dataJSON.fitbitUsername,
+        firstName: dataJSON.firstName,
+        lastName: dataJSON.lastName,
+        cellPhone: dataJSON.cellphone,
+        homeNumber: dataJSON.homephone,
+        emailAddress: dataJSON.email,
+        firstNameEC1: dataJSON.firstNameEC1,
+        lastNameEC1: dataJSON.lastNameEC1,
+        cellPhoneEC1: dataJSON.cellphoneEC1,
+        homeNumberEC1: dataJSON.homephoneEC1,
+        emailAddressEC1: dataJSON.emailEC1,
+        firstNameEC2: dataJSON.firstNameEC2,
+        lastNameEC2: dataJSON.lastNameEC2,
+        cellPhoneEC2: dataJSON.cellphoneEC2,
+        homeNumberEC2: dataJSON.homephoneEC2,
+        emailAddressEC2: dataJSON.emailEC2,
+      };
+
+      console.log(postData);
+
+      await axios
+        .post("http://localhost:4000/users/newUser", postData)
+        .then((res) => console.log(res.data));
+    };
+
+    axiosPostData();
   });
 
   return <Survey model={survey} />;
