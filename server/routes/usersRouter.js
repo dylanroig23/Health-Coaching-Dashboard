e0fbd43d73f4496fa11de50ccc96e7b748817876
Dylan Roig <roig.10@buckeyemail.osu.edu>
2024-03-31 18:57:10 -0400

    Basic Add User Auth
    Basic Add User Authorization with Fitbit

--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -19,14 +19,69 @@ const scopes = [
   "weight",
 ];
 require("dotenv/config");
+const { v4: uuidv4 } = require("uuid");
+const axios = require("axios");
 
-// callback from Fitbit Authorization
+/*
+  Callback from the Fitbit User Authorization
+*/
 usersRouter.get("newUser/callback", async (req, res) => {
-  // pass the userId as a query parameter and query it then update
-  // the mongoDB database value and input the access and refresh tokens
+  // get the authorization code, userId, and codeVerifier
+  const authorizationCode = req.query.code;
+  const userId = req.query.userId;
+  const codeVerifier = req.query.codeVerifier;
+
+  if (!authorizationCode) {
+    return res.status(400).send("Authorization Code not Provided.");
+  }
+  if (!userId) {
+    return res.status(400).send("UserId Not Provided.");
+  }
+  if (!codeVerifier) {
+    return res.status(400).send("Code Verifier Not Provided.");
+  }
+
+  // post to Fitbit API to get access token and refresh token
+  const getTokensUrl = "https://api.fitbit.com/oauth2/token";
+  const grantType = "authorization_code";
+  const url = new URL(getTokensUrl);
+  url.searchParams.append("client_id", process.env.CLIENT_ID);
+  url.searchParams.append("code", authorizationCode);
+  url.searchParams.append("code_verifier", codeVerifier);
+  url.searchParams.append("grant_type", grantType);
+  const authString = `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`;
+  const base64AuthString = Buffer.from(authString).toString("base64");
+  const response = await axios.post(url, null, {
+    headers: {
+      Authorization: base64AuthString,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  const tokensData = await response.json();
+
+  // update the user information in MongoDB
+  const mongoUser = await userSchema.Users.findById(userId);
+  if (!mongoUser) {
+    return res.status(404).send("User not found");
+  }
+  user.accessToken = tokensData.access_token;
+  user.refreshToken = tokensData.refresh_token;
+  user.lastUpdated = new Date();
+  user.fitbitUsername = tokensData.user_id;
+
+  try {
+    const saveUser = await mongoUser.save();
+    if (saveUser) {
+      res.send("User Updated Successfully");
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-// add a new user
+/*
+  Add a New User
+*/
 usersRouter.post("/newUser", async (req, res) => {
   const {
     name,
@@ -75,18 +130,10 @@ usersRouter.post("/newUser", async (req, res) => {
     emergencyContact2: emergencyContact2,
   };
 
-  // add the user and then do this work
-  // pass the MongoDB userID as a query parameter that can be grabbed
-  // on the callback
-  const codeVerifier = getCodeVerifier();
-  const codeChallenge = getCodeChallenge(codeVerifier);
-  const authorizationURL = getAuthorizationURL(
-    process.env.CLIENT_ID,
-    codeChallenge,
-    scopes
-  );
+  const userId = uuidv4();
 
   const userData = {
+    _id: userId,
     name: name,
     startDate: startDate,
     lastUpdated: new Date(),
@@ -98,10 +145,25 @@ usersRouter.post("/newUser", async (req, res) => {
 
   const newUser = new userSchema.Users(userData);
   const saveUser = await newUser.save();
-  if (saveUser) {
-    res.send(userData);
-  } else {
-    res.send("Failed to add new user.");
+
+  try {
+    const saveUser = await newUser.save();
+    if (saveUser) {
+      const codeVerifier = getCodeVerifier();
+      const codeChallenge = getCodeChallenge(codeVerifier);
+      const authorizationURL = getAuthorizationURL(
+        process.env.CLIENT_ID,
+        codeChallenge,
+        scopes
+      );
+      res.redirect(
+        `${authorizationURL}?userId=${userId}&codeVerifier=${codeVerifier}`
+      );
+    } else {
+      res.send("Failed to save new user.");
+    }
+  } catch (error) {
+    res.status(500).send("Error saving new user: " + error.message);
   }
 });
 
