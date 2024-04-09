4768da05b1222ce6cefcd83081e4ee2dbc329cf9
Noverbey <112339061+Noverbey@users.noreply.github.com>
2024-04-09 16:15:20 -0400

    Added Return button to all pages inside hamburger menu

--- health-coaching-dashboard-app/src/pages/SmartGoals.jsx
+++ health-coaching-dashboard-app/src/pages/SmartGoals.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import { SmartGoalsSurveyComponent } from "../components/SurveyComponent";
 import PageHeading from "../components/PageHeading";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Unstable_Grid2";
+import Button from "@mui/material/Button";
+
+// Styling for the MUI Component: Paper. This allows the use of the <DailyOverviewTab> tag below
+const JumpWeekDash = styled(Button)(() => ({
+  width: "100%",
+  height: "100%",
+  padding: 0,
+  color: "black",
+  "&:hover": {
+    color: "grey",
+  },
+}));
 
 const SmartGoals = ({ CLIENT_ID }) => {
   return (
     <>
       <PageHeading headingText={`Long-Term SMART Goals`} />
+      <Grid container spacing={2} justifyContent="center" padding="1rem">
+          <Grid xs={1.5}>
+            <JumpWeekDash href="/">
+              {" "}
+              <u>Return</u>
+            </JumpWeekDash>
+          </Grid>
+        </Grid>
       <SmartGoalsSurveyComponent />
     </>
   );
