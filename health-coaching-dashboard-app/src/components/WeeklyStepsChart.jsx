8d6f70a50eb76981bd2d6bda4fbf1531638b1bbf
Kevin Reese <123328498+kreese2327@users.noreply.github.com>
2024-04-15 14:28:10 -0400

    Average line for weekly steps chart

--- health-coaching-dashboard-app/src/components/WeeklyStepsChart.jsx
+++ health-coaching-dashboard-app/src/components/WeeklyStepsChart.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, ReferenceLine, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 import { getCurrentUser } from "../scripts/getCurrentUser";
 import { getWeeklyStepsData } from "../scripts/getWeeklyStepsData";
 
 const WeeklyStepsChart = () => {
   const [chartStepsData, setChartStepsData] = useState([]);
 
+  const [averageYValue, setAverageYValue] = useState();
   const [maxYValue, setMaxYValue] = useState();
   const handleMaxYChange = (event) => {
     const newValue = parseFloat(event.target.value);
@@ -18,6 +19,10 @@ const WeeklyStepsChart = () => {
         const currUser = await getCurrentUser();
         const weeklyStepsData = await getWeeklyStepsData(currUser);
         setChartStepsData(weeklyStepsData);
+
+        const totalSteps = weeklyStepsData.reduce((sum, dataPoint) => sum + dataPoint.steps, 0);
+        const averageSteps = totalSteps / weeklyStepsData.length;
+        setAverageYValue(averageSteps);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -29,12 +34,19 @@ const WeeklyStepsChart = () => {
   return (
     <>
       <ResponsiveContainer width="100%" height={300}>
-        <BarChart data={chartStepsData} margin={0}>
+        <BarChart data={chartStepsData} >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="day" />
           <YAxis domain={[0, maxYValue]} />
           <Tooltip />
           <Bar dataKey="steps" fill="#3bf563" />
+          <ReferenceLine 
+            y={averageYValue} 
+            label={{ value: 'Avg: ' + Math.floor(averageYValue), position: 'right'}}
+            strokeWidth={2} 
+            stroke="red" 
+            strokeDasharray="3 3" 
+          />
         </BarChart>
       </ResponsiveContainer>
       <label style={{ margin: '10px'}} >Max:</label>
@@ -46,6 +58,10 @@ const WeeklyStepsChart = () => {
         min={0}
         style={{ padding: '2px', width: '65px', marginBottom: '10px' }}
       />
+      <label style={{ marginRight: '20px', color: "#666666", float: "right" }}>
+          <span style={{ color: 'red' }}>Avg: </span>
+          {Math.floor(averageYValue)}
+      </label>
     </>
   );
 };
