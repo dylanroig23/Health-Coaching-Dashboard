1cae51680147a86919911980aa9e639834158eb2
Dylan Roig <roig.10@buckeyemail.osu.edu>
2024-04-16 20:46:30 -0400

    Contact Information Page
    Contact Information Page Cooresponding to input data

--- health-coaching-dashboard-app/src/pages/ContactInfo.jsx
+++ health-coaching-dashboard-app/src/pages/ContactInfo.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { ContactInfoSurveyComponent } from "../components/SurveyComponent";
 import PageHeading from "../components/PageHeading";
 import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Unstable_Grid2";
 import Button from "@mui/material/Button";
+import ContactInfoData from "../components/ContactInfoData";
 
 // Styling for the MUI Component: Paper. This allows the use of the <DailyOverviewTab> tag below
 const JumpWeekDash = styled(Button)(() => ({
@@ -20,15 +20,15 @@ const ContactInfo = ({ CLIENT_ID }) => {
   return (
     <>
       <PageHeading headingText="Client Contact Information" />
-        <Grid container spacing={2} justifyContent="center" padding="1rem">
-          <Grid xs={1.5}>
-            <JumpWeekDash href="/">
-              {" "}
-              <u>Return</u>
-            </JumpWeekDash>
-          </Grid>
+      <Grid container spacing={2} justifyContent="center" padding="1rem">
+        <Grid xs={1.5}>
+          <JumpWeekDash href="/">
+            {" "}
+            <u>Return</u>
+          </JumpWeekDash>
         </Grid>
-      <ContactInfoSurveyComponent />
+      </Grid>
+      <ContactInfoData />
     </>
   );
 };
